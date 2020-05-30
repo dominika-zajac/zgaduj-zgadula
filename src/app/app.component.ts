2090045a211175979346d4f0f6eeceeacc6ba695
Dominika Zając <dominikaz@qualtrics.com>
2020-05-30 13:57:02 +0200

    step-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'zgaduj-zgadula';
+  czyTrwaGra = false;
+  czyWygrana = false;
+  podpowiedz = "";
+  // null oznacza ze zmienna ma pustą wartość
+  liczbaKomputera = null; 
+  wpisanaLiczba = null;
+  min = 1;
+  max = 10;
+
+  rozpocznijGre() {
+    /*
+    Jakie wartości powinny mieć zmienne na początku naszej gry?
+    Co powinien zrobić komputer?
+    */
+  }
+
+  sprawdzLiczbe() {
+    /*
+    Ta funkcja powinna porównać liczbę wybraną przez komputer i wpisaną przez gracza,
+    a następnie ustawić odpowiednie wartości zmiennych:
+    - czyTrwaGra
+    - czyWygrana
+    - podpowiedz
+    */
+  }
+
+  wylosujNowaLiczbe(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
 }
